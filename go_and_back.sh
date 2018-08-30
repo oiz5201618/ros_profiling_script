f011181e8bc32e9335d8790fd2489a5c211d4209
oiz5201618 <oiz5201618@gmail.com>
2018-08-30 14:16:20 +0800

    Change go_and_back.sh path.

--- go_and_back.sh
+++ go_and_back.sh
@@ -1,15 +1,24 @@
 #!bin/bash
 
-read -p "times: " times
+#read -p "threads: " threads_num
+#read -p "times: " times
 
 python go_to.py 1.554 1.478 0 0 0 1
 
 python go_to.py -2.000122 -0.5 0 0 0 1
 
-record_id=`ps -fu $USER| grep "navigation" | grep -v "grep" | awk '{print $2}'`
+python go_to.py 1.512 -1.579 0 0 0 1
 
-kill -2 ${record_id}
+python go_to.py -1.517 1.581 0 0 0.932 0.363
 
-sleep 3
+python go_to.py 1.554 1.478 0 0 0 1
 
-mv /home/shaohua/Develope/data.txt /home/shaohua/Develope/Data/exp1/data_${times}Hz.txt
+python go_to.py -2.000122 -0.5 0 0 0 1
+
+#record_id=`ps -fu $USER| grep "navigation" | grep -v "grep" | awk '{print $2}'`
+
+#kill -2 ${record_id}
+
+#sleep 3
+
+#mv /home/shaohua/Develope/data.txt /home/shaohua/Develope/Data/exp2/thread${threads_num}/data_${times}Hz.txt
